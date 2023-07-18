3b1274a94a4111911c87411c349af2af038d2d9b
Ernest <owojori.tolulope@gmail.com>
2023-07-17 23:54:23 -0700

    about section update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,68 +1,97 @@
 import React from 'react'
-// import { Carousel } from "@material-tailwind/react";
-// import MyImage from "./Images"
-// import { BsChevronCompactLeft, BsChevronCompactRight} from "react-icons/bs";
-// import {Cover} from "../img/About1.png";
-// import Img from "./Images/About.png";
+
 
 const AboutPage = () => {
+    const images = ['https://trd-items.vercel.app/assets/images/slides/Slide3.png',
 
-    const slides = [
-        {
-            url: 'https://trd-items.vercel.app/assets/images/slides/Slide3.png'
-        },
-        {
-            url: 'https://trd-items.vercel.app/assets/images/slides/Slide5.png'
-        },
-        {
-            url: 'https://trd-items.vercel.app/assets/images/slides/Slide1.png'
-        },
-        
-    ]
-// console.log(Cover, Img);
+    'https://trd-items.vercel.app/assets/images/slides/Slide5.png', 
+
+    'https://trd-items.vercel.app/assets/images/slides/Slide1.png'
+]
+
+
+
+    const [currentImage, setCurrentImage] = React.useState(0);
+
+    const refs = images.reduce((acc, val, i) => {
+      acc[i] = React.createRef();
+      return acc;
+    }, {});
+  
+    const scrollToImage = i => {
+      setCurrentImage(i);
+     
+      refs[i].current.scrollIntoView({
+        //     Defines the transition animation.
+        behavior: 'smooth',
+        //      Defines vertical alignment.
+        block: 'nearest',
+        //      Defines horizontal alignment.
+        inline: 'start',
+      });
+    };
+  
+    const totalImages = images.length;
+  
+    const nextImage = () => {
+      if (currentImage >= totalImages - 1) {
+        scrollToImage(0);
+      } else {
+        scrollToImage(currentImage + 1);
+      };
+    };
+  
+    const previousImage = () => {
+      if (currentImage === 0) {
+        scrollToImage(totalImages - 1);
+      } else {
+        scrollToImage(currentImage - 1);
+      };
+    };
+  
+    const arrowStyle =
+      'absolute text-white text-2xl z-10 bg-black h-10 w-10 rounded-full opacity-75 flex items-center justify-center';
+  
+    
+    const sliderControl = isLeft => (
+      <button
+        type="button"
+        onClick={isLeft ? previousImage : nextImage}
+        className={`${arrowStyle} ${isLeft ? 'left-2' : 'right-2'}`}
+        style={{ top: '40%' }}
+      >
+        <span role="img" aria-label={`Arrow ${isLeft ? 'left' : 'right'}`}>
+          {isLeft ? '◀' : '▶'}
+        </span>
+      </button>
+    );
+  
     return (
-        <div className='max-w-[1400px] h-[780] w-full m-auto py-4 relative'>
+        <div className='w-screen flex justify-center'>
             
 
 
             <section className="bg-white dark:bg-gray-900">
                 
-                <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+                <div className="w-screen flex justify-center">
                     {/* <div style={{backgroundImage:`url(${slides[0].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
                     </div> */}
-                                                {/* <Carousel
-                                className="rounded-xl"
-                                navigation={({ setActiveIndex, activeIndex, length }) => (
-                                    <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-                                    {new Array(length).fill("").map((_, i) => (
-                                        <span
-                                        key={i}
-                                        className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                                            activeIndex === i ? "bg-white w-8" : "bg-white/50 w-4"
-                                        }`}
-                                        onClick={() => setActiveIndex(i)}
-                                        />
-                                    ))}
+                    <div  className='w-screen flex justify-center' >
+                                <div className="p-12 flex justify-center w-screen md:w-1/2 items-center">
+                            <div className="relative w-full">
+                                <div className="carousel">
+                                {sliderControl(true)}
+                                {images.map((img, i) => (
+                                    <div className="w-full flex-shrink-0" key={img} ref={refs[i]}>
+                                    <img src={img} className="w-full object-contain" />
                                     </div>
-                                )}
-                                >
-                                <img
-                                    src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-                                    alt="image 1"
-                                    className="h-full w-full object-cover"
-                                />
-                                <img
-                                    src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                                    alt="image 2"
-                                    className="h-full w-full object-cover"
-                                />
-                                <img
-                                    src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-                                    alt="image 3"
-                                    className="h-full w-full object-cover"
-                                />
-                                </Carousel> */}
-                    
+                                ))};
+                                {sliderControl()}
+                                </div>
+                            </div>
+                            </div>
+                            </div>
+                                              
                 </div>
 
                 <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
