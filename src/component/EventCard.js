4f008b528d16600d5abece0dfae62e64c60361e4
Hudhayfah <hoismail2017@gmail.com>
2023-07-20 17:00:25 +0100

    event revamp

--- src/component/EventCard.js
+++ src/component/EventCard.js
@@ -1,17 +1,16 @@
 import img from '../image/pic (4).png'
 
 const EventCard = ({title, date, description, loaction}) => {
-    console.log(title, 'title')
     return (
-        <div className="rounded-xl shadow-lg">
-            <div className="p-5 flex flex-col">
-                <div className="rounded-xl overflow-hidden">
-                    <img src={img}/>
+        <div>
+            <div className="rounded-xl shadow-lg">
+                <div className="p-2 flex flex-col">
+                    <div className="rounded-xl overflow-hidden">
+                        <img src={img} />
+                    </div>
                 </div>
-                <h5 className='text-2xl md:text-3xl font-medium mt-3'>{title}</h5>
-                <p className='text-slate-500 text-lg mt-3'>{description}</p>
-                <a href='#' className='text-center bg-blue-400 text-blue-700 py-2 rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-in'>View</a>
             </div>
+            <h5 className='text-1xl md:text-2xl font-small mt-3'>{title}</h5>
         </div>
     )
 }
